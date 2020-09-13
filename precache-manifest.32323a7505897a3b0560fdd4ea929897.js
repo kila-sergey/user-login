2882345aee4f4679115de17fa143770608043a1c
kila-sergey <kila-sergey@yandex.ru>
2020-09-13 23:10:29 +0300

    Updates

---
+++ precache-manifest.32323a7505897a3b0560fdd4ea929897.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809828520e3eaf71b0505cee00ef8d47",
+    "url": "/user-login/index.html"
+  },
+  {
+    "revision": "b8dbdfd70424ec6ad193",
+    "url": "/user-login/static/css/4.00fedd81.chunk.css"
+  },
+  {
+    "revision": "d53f58b938e747ffaa24",
+    "url": "/user-login/static/css/5.9eec129e.chunk.css"
+  },
+  {
+    "revision": "d35a25e7d745a06a3472",
+    "url": "/user-login/static/css/main.56d509af.chunk.css"
+  },
+  {
+    "revision": "d59b146caac489d5ea1e",
+    "url": "/user-login/static/js/2.ec81a9f7.chunk.js"
+  },
+  {
+    "revision": "71d8bc3ec214ae9da98355664aee2f9c",
+    "url": "/user-login/static/js/2.ec81a9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb565ec889ec90b5b583",
+    "url": "/user-login/static/js/3.a1d352a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/user-login/static/js/3.a1d352a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8dbdfd70424ec6ad193",
+    "url": "/user-login/static/js/4.5e6ce76e.chunk.js"
+  },
+  {
+    "revision": "d53f58b938e747ffaa24",
+    "url": "/user-login/static/js/5.c0dfcb26.chunk.js"
+  },
+  {
+    "revision": "d35a25e7d745a06a3472",
+    "url": "/user-login/static/js/main.1b6f1268.chunk.js"
+  },
+  {
+    "revision": "a4dd3b3a6e0acff8b903",
+    "url": "/user-login/static/js/runtime-main.dc166594.js"
+  }
+]);
